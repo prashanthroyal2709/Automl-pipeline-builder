792afd6f011e7d26fe4faa9953cceeff815ddef8
Prashanth <prashanthroyal317@gmail.com>
2026-01-14 22:26:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ import streamlit as st
 import pandas as pd
 
 st.write("HELLO STREAMLIT")
-st.write("Pandas imported successfully")
+
+uploaded_file = st.file_uploader("Upload CSV", type="csv")
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    st.write("CSV loaded")
+    st.dataframe(df.head())
